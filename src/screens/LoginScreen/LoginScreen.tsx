427b5cc64e00f41a79f7e5a0a38de3321261978c
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-04 17:08:53 +0100

    LoginScreen: Added UI components

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,11 +1,26 @@
+import { View, SafeAreaView, KeyboardAvoidingView } from "react-native";
 import Button from "../../components/Button/Button";
 import GlobalBody from "../../components/GlobalBody";
+import Input from "../../components/Input";
 import RegularText from "../../components/RegularText";
+import typography from "../../constants/typography";
+import { loginScreenStyles } from "./loginScreen.styles";
 
 const LoginScreen = () => {
   return (
-    <GlobalBody>
-      <RegularText>LoginScreen</RegularText>
+    <GlobalBody style={loginScreenStyles.wrapperView}>
+      <View style={loginScreenStyles.contentView}>
+        <View style={loginScreenStyles.title}>
+          <RegularText weight="bold" size={typography.h1}>
+            Dualis
+          </RegularText>
+        </View>
+
+        <View style={loginScreenStyles.inputFields}>
+          <Input label="Email" keyboardType="email-address" />
+          <Input label="Passwort" secureTextEntry={true} />
+        </View>
+      </View>
     </GlobalBody>
   );
 };
---
+++ src/screens/LoginScreen/loginScreen.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export const loginScreenStyles = StyleSheet.create({
+  wrapperView: {
+    justifyContent: "center",
+  },
+  contentView: {
+    paddingHorizontal: 20,
+    // backgroundColor: "lightblue",
+    justifyContent: "space-evenly",
+  },
+  title: {
+    // backgroundColor: "tomato",
+  },
+  inputFields: {
+    height: 150,
+    justifyContent: "space-around",
+    // backgroundColor: "orange",
+  },
+});
