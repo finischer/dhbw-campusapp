ed2e0c746329d3a7e38b2bfe9a1328978c7d06de
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-04 17:07:04 +0100

    Typography: Adjust font sizes

--- src/constants/typography/typography.ts
+++ src/constants/typography/typography.ts
@@ -1,7 +1,10 @@
+const BODY_FONT_SIZE = 16;
+
 export default {
-  h1: 19,
-  h2: 18,
-  h3: 16,
+  h1: BODY_FONT_SIZE * 1.6,
+  h2: BODY_FONT_SIZE * 1.4,
+  h3: BODY_FONT_SIZE * 1.2,
+  body: BODY_FONT_SIZE,
   Bold: "SourceSansProBold",
   Regular: "SourceSansProRegular",
 };
