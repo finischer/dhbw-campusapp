593b2984aa8c3db8c59f6ff81d45a99d76e12d5f
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-04 11:45:24 +0100

    Added dark and light colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,19 +1,29 @@
-const lightModeColors = {};
-
-const darkModeColors = {};
-
 const Colors = {
-  red: "#E2001A",
-  darkRed: "#D1001A",
-  gray: "#293743",
-  lightGray: "#eff1f2",
-  gray50: "#D5DADD",
-  gray80: "#9BA5AA",
-  grayDHBW: "#5D686E",
-  darkContainer: "#1A262E",
-  lightContainer: "#dcdfe0",
-  successGreenLight: "#60b643",
-  successGreenDark: "#43b643",
+  red600: "#D1001A",
+  red500: "#E2001A",
+  gray700: "#1A262E",
+  gray600: "#293743",
+  gray500: "#5D686E",
+  gray400: "#D5DADD",
+  green600: "#43b643",
+  green500: "#60b643",
+  white: "#eff1f2",
 };
 
-export { Colors };
+const darkModeColors = {
+  primary: Colors.gray700,
+  secondary: Colors.gray400,
+  accent: Colors.red600,
+  container: Colors.gray600,
+  success: Colors.green600,
+};
+
+const lightModeColors = {
+  primary: Colors.white,
+  secondary: Colors.gray600,
+  accent: Colors.red500,
+  container: Colors.gray400,
+  success: Colors.green500,
+};
+
+export { darkModeColors, lightModeColors };
