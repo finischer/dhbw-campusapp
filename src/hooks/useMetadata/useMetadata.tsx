47535920c946213c982aaafc475abbe52bb1f203
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-04 12:49:06 +0100

    Added color palette depends on theme mode

--- src/hooks/useMetadata/useMetadata.tsx
+++ src/hooks/useMetadata/useMetadata.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { darkModeColors, lightModeColors } from "../../styles/colors";
+import { IColors } from "../../styles/colors.types";
 import { IThemeTypes } from "./types/IThemeTypes";
 
 const useMetadata = () => {
   const [theme, setTheme] = useState<IThemeTypes>("light");
 
-  return { theme };
+  const colors: IColors = theme === "light" ? lightModeColors : darkModeColors;
+
+  return { theme, colors };
 };
 
 export default useMetadata;
