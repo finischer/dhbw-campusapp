0c0949ac97690c5b5b7b3d6d7e63eaa617f31cfc
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-05 08:29:20 +0100

    Device: Added device constants

---
+++ src/constants/device/device.ts
@@ -0,0 +1,4 @@
+import { Dimensions } from "react-native";
+
+export const WINDOW_HEIGHT: number = Dimensions.get("window").height;
+export const WINDOW_WIDTH: number = Dimensions.get("window").width;
