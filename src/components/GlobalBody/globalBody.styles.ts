699362fcca67bd6473ab142383db86626b1b2640
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-05 08:29:02 +0100

    GlobalBody: Adjust style

--- src/components/GlobalBody/globalBody.styles.ts
+++ src/components/GlobalBody/globalBody.styles.ts
@@ -1,11 +1,5 @@
-import { Dimensions } from "react-native";
-import {
-  HEADER_HEIGHT,
-  TAB_BAR_HEIGHT,
-} from "../../infrastructure/navigation/Navigation/config";
-
 export const globalBodyStyles = {
   paddingVertical: 10,
   paddingHorizontal: 20,
-  height: Dimensions.get("window").height - HEADER_HEIGHT - TAB_BAR_HEIGHT,
+  flex: 1,
 };
