6141624c181627b37698ffa13dbb5f8456f6d229
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-08 22:14:19 +0100

    Adjust return type of onClick function to void

--- src/components/Button/IButtonTypes.ts
+++ src/components/Button/IButtonTypes.ts
@@ -1,11 +1,13 @@
-type IButtonVariants = "text" | "contained" | "outlined";
+import { GestureResponderEvent } from "react-native";
+
+export type IButtonVariants = "text" | "contained" | "outlined";
 type IButtonSizes = "small" | "medium" | "large";
 
-type IButtonTypes = {
+export type IButtonTypes = {
   variant: IButtonVariants;
   leftIcon?: any;
   rightIcon?: any;
-  onClick?: () => void;
+  onClick?: any;
   children: React.ReactNode;
   size?: IButtonSizes;
 };
