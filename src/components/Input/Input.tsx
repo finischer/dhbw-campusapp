926db73c71a9d9ec2a5ff65dd2ddbb8d61adbf24
Niklas <66642266+finischer@users.noreply.github.com>
2022-12-08 23:59:21 +0100

    Input: Added border and text color

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,9 +3,13 @@ import React, { useState } from "react";
 import { IInputProps } from "./input.types";
 import { inputStyles } from "./input.styles";
 import RegularText from "../RegularText";
+import useMetadata from "../../hooks/useMetadata";
 
 const Input: React.FC<IInputProps> = ({ label, ...props }) => {
   const [isFocused, setIsFocused] = useState<boolean>(false);
+  const { colors } = useMetadata();
+  const borderColor = colors.secondary;
+  const textColor = colors.secondary;
 
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
@@ -15,7 +19,7 @@ const Input: React.FC<IInputProps> = ({ label, ...props }) => {
       <RegularText>{label}</RegularText>
       <TextInput
         {...props}
-        style={inputStyles.inputView}
+        style={[inputStyles.inputView, { borderColor, color: textColor }]}
         onFocus={handleFocus}
         onBlur={handleBlur}
       />
