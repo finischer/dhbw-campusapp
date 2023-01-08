1d64146d0dcee99b9d380e4705013989afff3085
Niklas <66642266+finischer@users.noreply.github.com>
2023-01-08 15:30:54 +0100

    Remove import

--- src/screens/MoreScreen/components/AppInfo/AppInfo.tsx
+++ src/screens/MoreScreen/components/AppInfo/AppInfo.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { appInfoStyles } from "./appInfo.styles";
 import RegularText from "../../../../components/RegularText";
