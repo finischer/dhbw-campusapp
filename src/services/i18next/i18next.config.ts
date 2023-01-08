6f30ef7ec2fd284a40e868adbf0c9533ddb784a2
Niklas <66642266+finischer@users.noreply.github.com>
2023-01-08 15:19:55 +0100

    Added auto language detector

--- src/services/i18next/i18next.config.ts
+++ src/services/i18next/i18next.config.ts
@@ -1,17 +1,18 @@
-import i18n from "i18next";
+import i18 from "i18next";
 import { initReactI18next } from "react-i18next";
 import de from "../../constants/translations/de";
 import en from "../../constants/translations/en";
+import RNLanguageDetector from "@os-team/i18next-react-native-language-detector";
 
 const resources = {
   de,
   en,
 };
 
-i18n.use(initReactI18next).init({
+i18.use(RNLanguageDetector).use(initReactI18next).init({
   compatibilityJSON: "v3",
   resources,
   fallbackLng: "en",
 });
 
-export default i18n;
+export default i18;
