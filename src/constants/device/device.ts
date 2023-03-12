dbbbf2f4b0e65a100a2a4480f6f3d5fe0984a7f3
Niklas <66642266+finischer@users.noreply.github.com>
2023-03-12 18:30:35 +0100

    Added constants about device

--- src/constants/device/device.ts
+++ src/constants/device/device.ts
@@ -1,4 +1,12 @@
+import * as Device from "expo-device";
 import { Dimensions } from "react-native";
+import { DeviceType } from "./device.types";
 
 export const WINDOW_HEIGHT: number = Dimensions.get("window").height;
 export const WINDOW_WIDTH: number = Dimensions.get("window").width;
+
+export const DEVICE: DeviceType = {
+  brand: Device.brand,
+  version: Device.osVersion,
+  modelName: Device.modelName,
+};
