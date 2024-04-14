e9947fc15b06450cd6cb5ee8c2798f844b966501
Niklas <66642266+finischer@users.noreply.github.com>
2024-04-14 21:16:10 +0200

    feat: menulist for 'erzbergstrasse'

--- src/api/html_scraper/DHBWKarlsruhe/DHBWKarlsruheRestaurantScraper.ts
+++ src/api/html_scraper/DHBWKarlsruhe/DHBWKarlsruheRestaurantScraper.ts
@@ -10,6 +10,8 @@ import { IRestaurantTypes } from "../restaurant/types/IRestaurantTypes";
 import { KarlsruheRestaurants } from "../restaurant/types/RestaurantTypes";
 import { KarlsruheRestaurantOptions } from "./types/KarlsruheRestaurants";
 import axios from "axios";
+import { MenuIconNames } from "../../../screens/RestaurantScreen/components/MenuIcon/menuIcon.types";
+import { PropsWithChildren } from "react";
 
 const TEST_MENUS: IMenuType[] = [
   {
@@ -69,11 +71,21 @@ const TEST_DAY_OPTIONS: IDayOptions[] = [
 // HINT: Only temporarily until user can choose his own role
 type TMP_Role = "student" | "guest" | "staff" | "pupils";
 
+enum PriceSelector {
+  Portion = "Portion",
+  Gramm = "gr",
+  Kilogramm = "kg",
+}
+
 export class DHBWKarlsruheRestaurantScraper extends RestaurantScraper {
   baseUrl: string;
   restaurants: KarlsruheRestaurants;
   price_html_classes: Record<TMP_Role, string>;
   role: TMP_Role;
+  html: any;
+  currRestaurant: KarlsruheRestaurantOptions | null;
+  currWeek: number | null;
+  weekCorrection: number;
 
   constructor(language: ILanguageOptions) {
     super();
@@ -89,19 +101,61 @@ export class DHBWKarlsruheRestaurantScraper extends RestaurantScraper {
     };
 
     this.role = "student";
+
+    this.currRestaurant = null;
+    this.html = null;
+    this.weekCorrection = 0;
+
+    this.currWeek = null;
+  }
+
+  extractPriceSelection(text: string) {
+    const SEARCH_KEYS = ["preis je"];
+
+    let priceSelector: string = PriceSelector.Portion; // DEFAULT
+
+    SEARCH_KEYS.forEach((key) => {
+      // const regex = new RegExp(`${key}\\s*(\\d+\\s*(g|kg))`, "gi");
+      const regex = new RegExp(`${key}\\s*(\\d+)\\s*(g|kg)`, "gi");
+
+      const match = regex.exec(text);
+      if (match) {
+        const weight = match.at(1) ?? "";
+        const weightUnit = match.at(2) ?? "-";
+
+        if (weightUnit === "g") {
+          priceSelector = PriceSelector.Gramm;
+        } else {
+          priceSelector = weightUnit;
+        }
+
+        if (weight) {
+          priceSelector = `${weight} ${priceSelector}`;
+        }
+
+        return;
+      }
+    });
+
+    return priceSelector;
   }
 
   async getHtmlOfRestaurant(
     restaurantKey: KarlsruheRestaurantOptions,
-    date: string | undefined = undefined
+    date: string | undefined = undefined,
+    calendarWeek: number | null = moment().isoWeek()
   ): Promise<IResponseTypes> {
+    if (this.html && this.currRestaurant === restaurantKey && calendarWeek === this.currWeek) {
+      return { msg: "successful", status: 200, data: this.html };
+    }
+
     const payloadDate = date || moment().format("YYYY-MM-DD");
 
     const pathToRestaurant = this.restaurants[restaurantKey];
-    const url = this.baseUrl + "/" + pathToRestaurant + "/?kw=16";
-    const res = await axios.post(url);
 
-    console.log("URL: ", url);
+    const url = this.baseUrl + "/" + pathToRestaurant + `/?kw=${calendarWeek}`;
+
+    const res = await axios.post(url);
 
     if (res.status !== 200) {
       return {
@@ -111,71 +165,123 @@ export class DHBWKarlsruheRestaurantScraper extends RestaurantScraper {
       };
     }
 
+    this.html = res.data;
+    this.currRestaurant = restaurantKey;
+    this.currWeek = calendarWeek;
     return { msg: "successful", status: 200, data: res.data };
   }
 
-  async getDayOptionsOfRestaurant(restaurantKey: KarlsruheRestaurantOptions): Promise<IResponseTypes> {
-    const res = await this.getHtmlOfRestaurant(restaurantKey);
+  async getDayOptionsOfRestaurant(
+    restaurantKey: KarlsruheRestaurantOptions,
+    week: number | null = null
+  ): Promise<IResponseTypes> {
+    const res = await this.getHtmlOfRestaurant(restaurantKey, undefined, week);
+
+    const $ = cheerio.load(res.data);
+
+    const optionList: IDayOptions[] = [];
+
+    $("ul.canteen-day-nav li a").each((_: number, dayElem: any) => {
+      const date = $(dayElem).attr("rel").trim();
+      const weekDay = moment(date).format("dddd");
+
+      const newDay: IDayOptions = {
+        weekDay,
+        date,
+      };
+
+      optionList.push(newDay);
+    });
 
     const response: IResponseTypes = {
       msg: "successful",
       status: 200,
-      data: TEST_DAY_OPTIONS,
+      data: optionList,
     };
 
     return response;
+  }
+
+  incrementWeek() {
+    if (this.currWeek) {
+      this.currWeek++;
+    }
   }
 
   async getMenuOfRestaurant(
     restaurantKey: KarlsruheRestaurantOptions,
     date: string
   ): Promise<IResponseTypes> {
-    const res = await this.getHtmlOfRestaurant(restaurantKey);
+    const dateCalendarWeek = moment(date).isoWeek();
+
+    const resDayOptions = await this.getDayOptionsOfRestaurant(restaurantKey, dateCalendarWeek);
+
+    const dayOptions = resDayOptions.data as IDayOptions[];
+
+    const dates = dayOptions.map((opt) => opt.date);
+
+    if (!dates.includes(date)) {
+      this.incrementWeek();
+    }
+
+    const dayId = dates.indexOf(date) + 1;
+
+    const res = await this.getHtmlOfRestaurant(
+      restaurantKey,
+      undefined,
+      dateCalendarWeek + this.weekCorrection
+    );
 
     if (!res.data) return res;
 
     const $ = cheerio.load(res.data);
     const restaurantName = $(".mensa_fullname").text();
 
-    console.log("restaurantName: ", restaurantName);
-    const $offer = $("#canteen_day_1").find(".mensatype_rows");
+    const $offer = $(`#canteen_day_${dayId}`).find(".mensatype_rows");
+
+    const offerList: IOfferListTypes = {
+      date,
+      menus: [],
+    };
 
     $offer.each((_: number, parentElem: any) => {
       const menuName = $(parentElem).find(".mensatype").text().trim();
 
-      const $menuDescriptions = $(parentElem).find(".menu-title");
+      const $menus = $(parentElem).find('[class^="mt-"]');
 
-      const menuDescriptions: string[] = [];
-      $menuDescriptions.each((_: number, descElem: any) => {
-        const desc = $(descElem).text().trim();
-        menuDescriptions.push(desc);
+      let tmpDesc = "";
+      $menus.each((_: number, menuElem: any) => {
+        const price_class = this.price_html_classes[this.role];
+        const menuPrice: string = $(menuElem).find(price_class).text().trim();
+        const menuDescription = $(menuElem).find(".menu-title").text().trim();
+
+        if (menuPrice.length === 0) {
+          tmpDesc += ` | ${menuDescription}`;
+        } else {
+          tmpDesc = menuDescription;
+
+          const menuPriceSelection = this.extractPriceSelection(menuDescription);
+          const menuIcons: Array<MenuIconNames> = [];
+
+          const iconName: MenuIconNames = "carrot"; // TODO: Find correct icon
+
+          const newMenu: IMenuType = {
+            menuName,
+            menuDescription,
+            menuIcons,
+            menuPrice,
+            menuPriceSelection,
+          };
+
+          offerList.menus.push(newMenu);
+        }
       });
-
-      const menuDescription = menuDescriptions.join(" | ");
-
-      const price_class = this.price_html_classes[this.role];
-      const menuPrice = $(parentElem).find(price_class).text().trim(); // price for students -> TODO: Show als
-
-      const newMenu: IMenuType = {
-        menuName,
-        menuDescription,
-        // menuIcons,
-        menuPrice,
-        // menuPriceSelection,
-      };
-
-      console.log("Menu: ", newMenu);
     });
 
-    const offerList: IOfferListTypes = {
-      date: "28.02.2023",
-      menus: TEST_MENUS,
-    };
-
     const restaurant: IRestaurantTypes = {
-      restaurantName: "Karlsruhe Mensa",
+      restaurantName,
       offer: offerList,
-      additivesDict: {},
+      additivesDict: {}, // TODO: extract additives from HTML
     };
 
     const response: IResponseTypes = {
