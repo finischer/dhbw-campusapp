1ace21aabd2b7f638c92fcf0a7fef3722eaa73db
Niklas <66642266+finischer@users.noreply.github.com>
2024-06-07 19:19:22 +0200

    style: new design for lectureInformationScreen

--- src/screens/LectureInformationScreen/LectureInformationScreen.tsx
+++ src/screens/LectureInformationScreen/LectureInformationScreen.tsx
@@ -1,41 +1,44 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { View } from 'react-native';
-import Modal from '../../components/Modal/Modal';
-import RegularText from '../../components/RegularText/RegularText';
-import { ParamList } from '../../infrastructure/navigation/Navigation/navigation.types';
-import LectureInformation from './components/LectureInformation';
-import { lectureInformationStyles } from './lectureInformationScreen.styles';
-
+import { RouteProp, useRoute } from "@react-navigation/native";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { ScrollView, View } from "react-native";
+import Modal from "../../components/Modal/Modal";
+import RegularText from "../../components/RegularText/RegularText";
+import { ParamList } from "../../infrastructure/navigation/Navigation/navigation.types";
+import LectureInformation from "./components/LectureInformation";
+import { lectureInformationStyles } from "./lectureInformationScreen.styles";
 
 const LectureInformationScreen = () => {
-    const { t } = useTranslation();
-    const { params: changes } = useRoute<RouteProp<ParamList, "LectureDetails">>();
-    const oldLecture = changes.oldLecture;
-    const newLecture = changes.newLecture;
+  const { t } = useTranslation();
+  const { params: changes } = useRoute<RouteProp<ParamList, "LectureDetails">>();
+  const oldLecture = changes.oldLecture;
+  const newLecture = changes.newLecture;
 
-    const modalTitle = t("calendarScreen:lectureInformationTitle")
-    const beforeTitle = t("common:before")
-    const currentTitle = t("common:current")
+  const modalTitle = t("calendarScreen:lectureInformationTitle");
+  const beforeTitle = t("common:before");
+  const currentTitle = t("common:current");
 
-    return (
-        <Modal title={modalTitle}>
-            <View style={lectureInformationStyles.container}>
-                {/* Before View */}
-                <LectureInformation title={beforeTitle} lecture={oldLecture} keyChanges={changes.keyChanges} />
+  return (
+    <Modal title={modalTitle}>
+      <ScrollView>
+        <View style={lectureInformationStyles.container}>
+          {/* Before View */}
+          <LectureInformation
+            title={beforeTitle}
+            lecture={oldLecture}
+            keyChanges={changes.keyChanges}
+          />
 
-                {/* After View */}
-                <LectureInformation title={currentTitle} lecture={newLecture} keyChanges={changes.keyChanges} />
+          {/* After View */}
+          <LectureInformation
+            title={currentTitle}
+            lecture={newLecture}
+            keyChanges={changes.keyChanges}
+          />
+        </View>
+      </ScrollView>
+    </Modal>
+  );
+};
 
-                {/* Maintenance Design hint */}
-                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                    <RegularText style={{ opacity: 0.3 }}>{t("common:maintenanceHintDesign")}</RegularText>
-                </View>
-            </View>
-        </Modal>
-    )
-}
-
-export default LectureInformationScreen
-
+export default LectureInformationScreen;
