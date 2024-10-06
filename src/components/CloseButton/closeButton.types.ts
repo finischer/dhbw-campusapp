739bed84f41e64eb2b58f4a4819ed490f62478a6
Niklas Fischer <niklasfischer0@gmail.com>
2024-10-06 13:57:40 +0200

    UI: Check icon instead of X button in Modal

--- src/components/CloseButton/closeButton.types.ts
+++ src/components/CloseButton/closeButton.types.ts
@@ -1,3 +1,6 @@
+import { FeatherIconName } from "../../services/expo-vector-icons/expo-vector-icons.types";
+
 export type ICloseButtonProps = {
   onClick?(): void;
+  iconName: FeatherIconName;
 };
